2c75d6f05706ac384e01708451f0e30b0b6807f4
Paweł Susfał <susfal@student.agh.edu.pl>
2021-01-26 13:23:00 +0100

    add method to generate list with stream

--- src/main/java/com/epam/prejap/ess/helpers/collectionsfill/CollectionFillTimeTester.java
+++ src/main/java/com/epam/prejap/ess/helpers/collectionsfill/CollectionFillTimeTester.java
@@ -2,10 +2,19 @@ package com.epam.prejap.ess.helpers.collectionsfill;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CollectionFillTimeTester {
     private static final int NUMBER_OF_ELEMENTS = 1_000_000;
 
+
+    private static List<Object> generateListUsingStream(){
+        return Stream.generate(String::new)
+                .limit(NUMBER_OF_ELEMENTS)
+                .map(s -> new Object())
+                .collect(Collectors.toList());
+    }
 
     private static List<Object> generateListUsingLoop(){
         List<Object> list = new ArrayList<>();
