d4cefd6bd8bd0469b36050e7cf7fa1d52da22fa1
Paweł Susfał <susfal@student.agh.edu.pl>
2021-01-26 13:22:24 +0100

    add method to generate list with for loop

--- src/main/java/com/epam/prejap/ess/helpers/collectionsfill/CollectionFillTimeTester.java
+++ src/main/java/com/epam/prejap/ess/helpers/collectionsfill/CollectionFillTimeTester.java
@@ -1,4 +1,18 @@
 package com.epam.prejap.ess.helpers.collectionsfill;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectionFillTimeTester {
+    private static final int NUMBER_OF_ELEMENTS = 1_000_000;
+
+
+    private static List<Object> generateListUsingLoop(){
+        List<Object> list = new ArrayList<>();
+        for(int i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+            list.add(new Object());
+        }
+
+        return list;
+    }
 }
