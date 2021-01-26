7ff14d1cbb6bc26bafd1be095d61d3fd5d6447d0
Paweł Susfał <susfal@student.agh.edu.pl>
2021-01-26 14:00:05 +0100

    add main method to test how long it takes different methods to create a list

--- src/main/java/com/epam/prejap/ess/helpers/collectionsfill/CollectionFillTimeTester.java
+++ src/main/java/com/epam/prejap/ess/helpers/collectionsfill/CollectionFillTimeTester.java
@@ -9,7 +9,53 @@ import java.util.stream.Stream;
 
 public class CollectionFillTimeTester {
     private static final int NUMBER_OF_ELEMENTS = 1_000_000;
+    private static final int NUMBER_OF_TRIES = 1;
 
+    public static void main(String[] args) {
+        long startTime, endTime;
+        long timeArraysFill = 0, timeNCopies = 0, timeStream = 0, timeForLoop = 0, timeListFill = 0;
+
+        for(int i = 0; i < NUMBER_OF_TRIES; i++) {
+
+            startTime = System.currentTimeMillis();
+            ArrayList<Object> arrayListFromArraysFill = new ArrayList<>(generateListUsingArraysFill());
+            endTime = System.currentTimeMillis();
+            timeArraysFill += endTime - startTime;
+
+            startTime = System.currentTimeMillis();
+            ArrayList<Object> arrayListFromListFill = new ArrayList<>(generateListUsingFill());
+            endTime = System.currentTimeMillis();
+            timeListFill += endTime - startTime;
+
+            startTime = System.currentTimeMillis();
+            ArrayList<Object> arrayListFromNCopies = new ArrayList<>(generateListUsingCollectionsNCopies());
+            endTime = System.currentTimeMillis();
+            timeNCopies += endTime - startTime;
+
+
+            startTime = System.currentTimeMillis();
+            ArrayList<Object> arrayListFromStream = new ArrayList<>(generateListUsingStream());
+            endTime = System.currentTimeMillis();
+            timeStream += endTime - startTime;
+
+
+            startTime = System.currentTimeMillis();
+            ArrayList<Object> arrayListFromLoop = new ArrayList<>(generateListUsingLoop());
+            endTime = System.currentTimeMillis();
+            timeForLoop += endTime - startTime;
+        }
+
+        String resultMessage = "Test: list of " + NUMBER_OF_ELEMENTS +
+                " elements, average time in ms after " + NUMBER_OF_TRIES + " tries" + "\n" +
+                "Methode\ttime" + "\n" +
+                "NCopies " + timeNCopies / NUMBER_OF_TRIES + "\n" +
+                "Arrays.fill " + timeArraysFill / NUMBER_OF_TRIES + "\n" +
+                "List.fill " + timeListFill / NUMBER_OF_TRIES + "\n" +
+                "For loop " + timeForLoop / NUMBER_OF_TRIES + "\n" +
+                "stream " + timeStream / NUMBER_OF_TRIES + "\n";
+
+        System.out.println(resultMessage);
+    }
 
     private static List<Object> generateListUsingCollectionsNCopies(){
         return Collections.nCopies(NUMBER_OF_ELEMENTS, new Object());
@@ -43,3 +89,13 @@ public class CollectionFillTimeTester {
         return list;
     }
 }
+/*
+result example
+Test: list of 1000000 elements, average time in ms after 1 tries
+Methode	time
+NCopies 15
+Arrays.fill 18
+List.fill 22
+For loop 87
+stream 106
+ */
